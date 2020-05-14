9c681c63190080d5d5954db9e3968d2983646d26
Jakob <ytdalnert@gmail.com>
2020-05-14 14:45:24 +0200

    jag börja ba

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html>
+<title>Systemvetarkarriär</title>
+    <head>
+        <script src="https:ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <h1 >
+            <meta charset="utf-8"/>   
+        </h1>
+        <link rel="stylesheet" type ="text/css" href ="Lab1_css/StyleLab1.css">
+        
+    </head>
+    <body>
+    
+    </body>
+
+</html>
