f0612c26e3bb7bf7a7999c0796e8d3da5b7b566c
unknown <carl.lundin96gmail.com>
2020-05-15 11:48:49 +0200

    Calles ändring

---
+++ Startpage_logedIn.php
@@ -0,0 +1,41 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html>
+<title>Systemvetarkarriär_inloggad</title>
+    <head>
+        <script src="https:ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <h1 >
+            <meta charset="utf-8"/>   
+        </h1>
+        <link rel="stylesheet" type ="text/css" href ="css/style.css">
+        
+    </head>
+    <a>
+    <div>
+    <div class="Log_out_knapp">
+    <button onclick="return Metod()" type="submit" value="save" name="log_out">Logga ut</button>
+    </div>
+</a>
+    <body>
+    <div class="header">
+            <h1>Systemvetarkarriär</h1>
+            <p>Välkommen "användare"</p>
+        </div>
+
+
+    </body>
+    <body>
+    </br>
+    <div>
+    <div class="Kategori_knapp">
+    <button onclick="return Metod()" type="submit" value="save" name="kategori">Se samtliga karriärsvägar</button>
+    </div>
+
+    <div class="karriär_knapp">
+    <button onclick="return Metod()" type="submit" value="save" name="Kkompas">Karriär Kompassen</button>>
+    </div>
+
+    </body>
+
+</html>
