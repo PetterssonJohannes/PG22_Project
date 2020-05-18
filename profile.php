29d96c0e9b1f0ee030b77fe6a4b3968b16ecc99b
Johannes <pettersson.johannes@gmail.com>
2020-05-18 09:42:51 +0200

    Header i profile.php

---
+++ profile.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+        <script scr="js/js.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <h1>
+            <meta charset="utf-8"/>   
+        </h1> 
+    </head>
+
+    <body>
+        <div class="header">
+            <div name="Rubrik" class="rubrik">
+                <h1>Systemvetarkarriär</h1><br>
+            </div>       
+        </div>
+    </body>
+</html>
