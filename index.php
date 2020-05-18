1ae788c83efb65ff30f7334bd08eb562878ade81
Jakob <ytdalnert@gmail.com>
2020-05-18 13:09:46 +0200

    ny mapp

--- index.php
+++ index.php
@@ -6,31 +6,34 @@
     <head>
         <script src="https:ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <h1 >
-            <meta charset="utf-8"/>   
+        <meta charset="utf-8"/>   
         </h1>
+        
         <link rel="stylesheet" type ="text/css" href ="css/style.css">
         
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     </head>
     <body>
     
-    <div class="header">
-        <div name="Rubrik" class="rubrik">
-            <h1>Systemvetarkarriär</h1>
-        </div>       
+        <div class="header">
+            <div name="Rubrik" class="rubrik">
+                <h1>Systemvetarkarriär</h1>
+            </div>       
     
             
         
-    <div name="Buttons" class="Btns">
-        <div>
-            <form action="" name="RegBtn">
-            <input type="submit" value="Registrera dig"></form>
+            <div name="Buttons" class="Btns">
+                <div>
+                    <form action="" name="RegBtn">
+                    <input type="submit" value="Registrera dig"></form>
+                </div>
+                <div>
+                    <form action="" name="LogBtn">
+                    <input type="submit" value="Logga in"></form>
+                </div>
+            </div>
         </div>
-        <div>
-            <form action="" name="LogBtn">
-            <input type="submit" value="Logga in"></form>
-        </div>
-    </div>
-    </div>
 
     </body>
 
