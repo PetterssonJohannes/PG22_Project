a22ac85aa97b197d0842ce7f7f35c5e9e2673927
Linus Häggkvist <linus.haggkvist@gmail.com>
2020-05-18 13:21:55 +0200

    sdas

--- validateLogin.php
+++ validateLogin.php
@@ -1,12 +1,11 @@
 <?php include "db.php";
-if(!Login($_POST["username"], $_POST["password"])){
-    header("Location: login.php");
-    echo"Username and password does not match";
+if(Login($_POST["username"], $_POST["password"])){
+    header("Location: logedIn.php");
 }
 else{
     /* header("Location : startPage.php"); 
     alternativt header("successfulLogin");
     */
-    header("Location: index.php");
+    echo"Username and password does not match";
 }
 ?>
