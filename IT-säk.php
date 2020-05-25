c6d104ca292626025c02ecee61a3ca7a834551c6
dalnert <65162278+dalnert@users.noreply.github.com>
2020-05-25 15:50:03 +0200

    Skapat kategorisidor

---
+++ IT-säk.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<title>Systemvetarkarriär - IT-säkerhet</title>
+    <head>
+        <link rel="stylesheet" type ="text/css" href ="Lab3ES_css/StyleLab3ES.css">
+        <h1 >
+            <meta charset="utf-8"/>   
+        </h1>
+    </head>
+    <body id="It_sak">
+
+
+    </body>
+</html>
