778223dd6bdfc70dddd4a9083a69597e7d70f489
Linus Häggkvist <linus.haggkvist@gmail.com>
2020-06-01 17:34:58 +0200

    assu victuur

--- termsofservice.php
+++ termsofservice.php
@@ -20,12 +20,23 @@
         <div class="infobox">
             <div>
                 <h2>Här ska vi ha våra regler</h2>
-                <div>
-                    <?php
-                        echo file_get_contents( "TEST TEST TEST.txt" );
-                    ?>
-                </div>
-            </div>
+                HUR kommer vi använda er sparade data?
+                Vi kommer endast att spara hashad mail, användarnamn och email. Email-adressen kommer endast användas om vi behöver kontakta en viss användare av någon av följande anledningar:
+                1.Användaren har blivit avstängd på grund av att den brutit mot forumreglerna.
+                2. Användaren har svurit på forumet.
+
+                Vi kommer inte att använda kontaktuppgifterna till marknadsföring.
+                
+                Om användaren vill att vi ska ta bort någon av dess uppgifter kan de kontakta oss via carl.lundin@systembolaget.se
+
+                Forumregler:
+                Man får inte skriva svordomar eller taskmörtiga kommentarer till andra användare.
+                Om en användare bryter mot formreglerna kommer en admin att ta bort kommentarerna och användaren kan bli avstängd.
+
+
+                
+
+                
         </div>
     </body>
 </html>
